867f39e35072734eb1259627772b18ba4aa69c98
victer-star <79968621+victer-star@users.noreply.github.com>
2021-05-03 18:35:57 +0900

    静的ページ（利用規約など）を作成

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,14 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-      config.generators do |g|
-        g.assets false          # CSS, JavaScriptファイルは生成しない
-        g.test_framework false  # testファイルは生成しない
-      end
-
+    config.generators do |g|
+      g.assets false          # CSS, JavaScriptファイルは生成しない
+      g.test_framework false  # testファイルは生成しない
       g.test_framework :rspec,
-          controller_specs: false,
-          view_specs: false,
-          helper_specs: false,
-          routing_specs: false
-      end
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
