00395bb46ece47fc03066e651d4f4d356e14bbb1
victer-star <79968621+victer-star@users.noreply.github.com>
2021-05-11 16:54:07 +0900

    とりあえずここまで

--- Gemfile
+++ Gemfile
@@ -55,7 +55,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'webdrivers'
+  gem 'webdrivers', require: !ENV['SELENIUM_DRIVER_URL']
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,27 @@
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+Capybara.register_driver :remote_chrome do |app|
+  url = ENV['SELENIUM_DRIVER_URL']
+  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions' => {
+      'args' => [
+        'no-sandbox',
+        'headless',
+        'disable-gpu',
+        'window-size=1680,1050'
+      ]
+    }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
 end
 
-Capybara.javascript_driver = :chrome_headless
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    #driven_by :selenium_chrome_headless
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.app_host = "http://#{Capybara.server_host}"
+    driven_by :remote_chrome
+  end
+end
