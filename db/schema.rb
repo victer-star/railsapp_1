6356b54e45ccf177c51bb6460b84ce323d40eea1
victer-star <79968621+victer-star@users.noreply.github.com>
2021-05-14 17:40:08 +0900

    フォロー機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_035629) do
+ActiveRecord::Schema.define(version: 2021_05_14_072141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
 
   create_table "trainings", force: :cascade do |t|
     t.string "name"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+
+  context "フォロー機能" do
+    it "フォローとフォロー解除が正常に動作すること" do
+      expect(user.following?(other_user)).to be_falsey
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+      expect(other_user.followed_by?(user)).to be_truthy
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
+  end
 
   context "バリデーション" do
     it "名前、メールアドレスがあれば有効な状態であること" do
